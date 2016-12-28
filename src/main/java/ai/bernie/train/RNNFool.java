067974bd26419f7d4b49b43d89ff26472c79af05
Justin Long <crockpotveggies@users.noreply.github.com>
2016-12-27 22:46:19 -0800

    Update comments.

--- src/main/java/ai/bernie/train/RNNFool.java
+++ src/main/java/ai/bernie/train/RNNFool.java
@@ -26,10 +26,13 @@ import java.nio.charset.Charset;
 import java.util.Random;
 
 /**
- * This example trains a RNN. WHen trained we only have to put the first
- * character of LEARNSTRING to the RNN, and it will recite the following chars
+ * This example trains a RNN. Using the Lotto 649 dataset, the RNN
+ * will start to predict sequential numbers that resemble "winning"
+ * lottery numbers.
  *
- * @author Peter Grossmann
+ * Partly derived from the Shakespeare character generator.
+ *
+ * @author crockpotveggies
  */
 public class RNNFool {
   private static final Logger logger = LoggerFactory.getLogger(RNNFool.class);
@@ -40,16 +43,14 @@ public class RNNFool {
     int lstmLayerSize2 = 50;
     int lstmLayerSize3 = 50;
     int lstmLayerSize4 = 50;
-    int miniBatchSize = 32;						//Size of mini batch to use when  training
-    int exampleLength = 600;					//Length of each training example sequence to use. This could certainly be increased
-    int tbpttLength = 6;                       //Length for truncated backpropagation through time. i.e., do parameter updates ever 50 characters
-    int numEpochs = 8;							//Total number of training epochs
-    int generateSamplesEveryNMinibatches = 100;  //How frequently to generate samples from the network? 1000 characters / 50 tbptt length: 20 parameter updates per minibatch
-    int nSamplesToGenerate = 4;					//Number of samples to generate after each training epoch
-    int nCharactersToSample = 6;				//Length of each sample to generate
-    String generationInitialization = "\n";		//Optional character initialization; a random character is used if null
-    // Above is Used to 'prime' the LSTM with a character sequence to continue/complete.
-    // Initialization characters must all be in CharacterIterator.getMinimalCharacterSet() by default
+    int miniBatchSize = 32;
+    int exampleLength = 600;
+    int tbpttLength = 6;
+    int numEpochs = 8;
+    int generateSamplesEveryNMinibatches = 100;
+    int nSamplesToGenerate = 4;
+    int nCharactersToSample = 6;
+    String generationInitialization = "\n";
     Random rng = new Random(12345);
 
 
@@ -68,8 +69,6 @@ public class RNNFool {
         .rmsDecay(0.96)
         .seed(12345)
         .regularization(true)
-//        .l1(0.0001)
-//        .l2(0.00001)
         .dropOut(0.7)
         .weightInit(WeightInit.XAVIER)
         .updater(Updater.RMSPROP)
@@ -80,8 +79,6 @@ public class RNNFool {
         .layer(2, new GravesLSTM.Builder().nIn(lstmLayerSize2).nOut(lstmLayerSize3).build())
         .layer(3, new GravesLSTM.Builder().nIn(lstmLayerSize3).nOut(lstmLayerSize4).build())
         .layer(4, new GravesLSTM.Builder().nIn(lstmLayerSize4).nOut(lstmLayerSize3).build())
-//        .layer(5, new GravesLSTM.Builder().nIn(lstmLayerSize3).nOut(lstmLayerSize2)
-//            .activation("tanh").build())
         .layer(5, new RnnOutputLayer.Builder(LossFunction.MCXENT).activation(Activation.SOFTMAX)        //MCXENT + softmax for classification
             .nIn(lstmLayerSize3).nOut(nOut).build())
         .backpropType(BackpropType.TruncatedBPTT).tBPTTForwardLength(tbpttLength).tBPTTBackwardLength(tbpttLength)
